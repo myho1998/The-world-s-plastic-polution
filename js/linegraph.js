e5c5db0d0ea3f182078a76565e623f8c294388e5
My Ho <myho@uw.edu>
2023-03-10 14:42:50 -0800

    ad

--- js/linegraph.js
+++ js/linegraph.js
@@ -3,15 +3,16 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Year', 'Plastic Production', 'Plastic Recycling'],
-    ['1950',  2, 0],
-    ['1960',  8, 0],
-    ['1970',  35, 0],
-    ['1980',  70, 0],
-    ['1990',  120, 5],
-    ['2000',  213, 20],
-    ['2010',  313, 54],
-    ['2019',  459.75, 96],
+    ['Year', 'Plastic Production', 'Plastic Recycling', 'Europe Plastic Production'],
+    ['1950',  2, 0, 0.35],
+    ['1960',  8, 0, 16],
+    ['1970',  35, 0, 19.8],
+    ['1976',  50, 0, 24.8],
+    ['1980',  70, 0, 27.4],
+    ['1990',  120, 5, 39.8],
+    ['2000',  213, 20, 56.1],
+    ['2010',  313, 54, 57],
+    ['2019',  459.75, 96, 56.2],
   ]);
 
   var options = {
