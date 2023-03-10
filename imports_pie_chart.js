fe0a3c4aecadd66aac74d5ff4f0b71b14ad2d3b2
Truong Le <truongle@uw.edu>
2023-03-10 02:26:56 -0800

    Fixed pie chart

---
+++ imports_pie_chart.js
@@ -0,0 +1,107 @@
+// Loads data from global waste exports in 2010 csv
+function parseData(selection, createGraph) {
+    Papa.parse("./assets/global_waste_imports_2010.csv", {
+        download: true,
+        complete: function(results) {
+            createGraph(selection, results.data);
+        }
+    });
+}
+
+
+function createGraph(selection, data) {
+    var Country = [];
+    var Imports = [];
+    // All non-country entries in the global_waste_exports_2010.csv file
+    // Separated so we can easily do different views
+    let continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania"];
+    let income_levels = ["Low-income countries", "Lower-middle-income countries", "Upper-middle-income countries",
+        "High-income countries"];
+
+    // Not the world's most efficient code, but it works
+    switch (selection) {
+        case "Country":
+            for (var i = 0; i < data.length; i++) {
+                // Just countries
+                if (!continents.includes(data[i][0]) && !income_levels.includes(data[i][0]) && data[i][0] != "World") {
+                    Country.push(data[i][0]);
+                    Imports.push(data[i][4]);
+                } else {
+                    console.log("Excluded: " + data[i][0]);
+                }
+            }
+            break;
+        case "Continent":
+            // Just continents
+            for (var i = 0; i < data.length; i++) {
+                if (continents.includes(data[i][0])) {
+                    Country.push(data[i][0]);
+                    Imports.push(data[i][4]);
+                } else {
+                    console.log("Excluded: " + data[i][0]);
+                }
+            }
+            break;
+        case "Income Level":
+            // Just income levels
+            for (var i = 0; i < data.length; i++) {
+                if (income_levels.includes(data[i][0])) {
+                    Country.push(data[i][0]);
+                    Imports.push(data[i][4]);
+                } else {
+                    console.log("Excluded: " + data[i][0]);
+                }
+            }
+            break;
+        default:
+            console.log("Error: default value reached in switch statement");
+    }
+
+    // Creates the pie chart
+    var summary = c3.generate({
+        bindto: '#chart',
+        data: {
+            rows: [
+                Country,
+                Exports
+            ],
+            type: 'pie',
+            labels: true,
+            onclick: function(d, i) {
+                console.log("onclick", d, i);
+            },
+            onmouseover: function(d, i) {
+                console.log("onmouseover", d, i);
+            },
+            onmouseout: function(d, i) {
+                console.log("onmouseout", d, i);
+            }
+        },
+        
+        // Creates label to show number of exports for each countries when hovered
+        tooltip: {
+            format: {
+                value: function (value, ratio, id) {
+                    return `${value} Imports`;
+                }
+            }
+        },
+
+        legend: {
+            show: false
+        }
+    });
+
+}
+
+
+function selectView(selection) {
+    var elem = document.getElementById("pie_selector");
+    if (elem.innerHTML != selection) {
+        elem.innerHTML = selection;
+        parseData(selection, createGraph);
+    }
+}
+
+
+parseData("Country", createGraph);
