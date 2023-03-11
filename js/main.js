95d77d038dd8dfecd108b6db01955d78eb50753f
lil-ds <lilyshen@uw.edu>
2023-03-10 21:50:55 -0800

    adding legend

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ mapboxgl.accessToken =
             container: 'map', // container ID
             style: 'mapbox://styles/lillly02/clf33ygu3001701phfouo9jh9',
             zoom: 2, // starting zoom
-            center: [-100, 40] // starting center
+            center: [50, 40] // starting center
         });
-        const grades = [1000, 5000, 10000, 50000, 100000],
-            colors = ['rgb(208,209,230)', 'rgb(103,169,207)', 'rgb(1,108,89)', 'rgb(1,59,48)', 'rgb(0,18,14)'],
-            radii = [5, 10, 15, 20, 40];
-
+        const grades = [50, 70, 90, 110, 130],
+        colors = ['rgb(208,209,230)', 'rgb(103,169,207)', 'rgb(1,108,89)', 'rgb(1,59,48)', 'rgb(0,18,14)'],
+        radii = [5, 10, 15, 20, 40];
+        
         map.on('load', () => { 
             map.addSource('us-covid-2020-counts', {
                 type: 'geojson',
-                data: 'assets/us-covid-2020-counts.json'
+                data: 'assets/data_for_choropleth.js'
             });
             map.addLayer({
                 'id': 'covid-2020-counts',
@@ -22,7 +22,7 @@ mapboxgl.accessToken =
                 'paint': {
                     // increase the radii of the circle as cases value increases
                     'circle-radius': {
-                        'property': 'cases',
+                        'property': 'pWastePercap2016',
                         'stops': [
                             [grades[0], radii[0]],
                             [grades[1], radii[1]],
@@ -33,7 +33,7 @@ mapboxgl.accessToken =
                     },
                     // change the color of the circle as cases value increases
                     'circle-color': {
-                        'property': 'cases',
+                        'property': 'pWastePercap2016',
                         'stops': [
                             [grades[0], colors[0]],
                             [grades[1], colors[1]],
@@ -48,35 +48,32 @@ mapboxgl.accessToken =
                 }
             });
             // click on tree to view magnitude in a popup
-            map.on('click', 'covid-2020-counts', (event) => {
-                new mapboxgl.Popup()
-                    .setLngLat(event.features[0].geometry.coordinates)
-                    .setHTML(`<strong>Counts:</strong> ${event.features[0].properties.cases}`)
-                    .addTo(map);
-            });
+// create legend object, it will anchor to the div element with the id legend.
+const legend = document.getElementById('legend');
+
+//set up legend grades and labels
+var labels = ['<strong>Size</strong>'], vbreak;
+//iterate through grades and create a scaled circle and label for each
+for (var i = 0; i < grades.length; i++) {
+    vbreak = grades[i];
+    // you need to manually adjust the radius of each dot on the legend 
+    // in order to make sure the legend can be properly referred to the dot on the map.
+    dot_radius = 2 * radii[i];
+    labels.push(
+        '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radius +
+        'px; height: ' +
+        dot_radius + 'px; "></i> <span class="dot-label" style="top: ' + dot_radius / 2 + 'px;">' + vbreak +
+        '</span></p>');
+
+}
+
+// combine all the html codes.
+legend.innerHTML = labels.join('');
+
+
         });
 
-        const legend = document.getElementById('legend');
-        //set up legend grades and labels
-        var labels = ['<FONT COLOR="#FFFFFF"><strong>Covid Case Count</strong>'],
-            vbreak;
-        //iterate through grades and create a scaled circle and label for each
-        for (var i = 0; i < grades.length; i++) {
-            vbreak = grades[i];
-            // you need to manually adjust the radius of each dot on the legend 
-            // in order to make sure the legend can be properly referred to the dot on the map.
-            dot_radii = 2 * radii[i];
-            labels.push(
-                '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
-                'px; height: ' +
-                dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
-                '</span></p>');
-        }
-        // add the data source
-        const source =
-            '<p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">NYT</a></p>';
-        // combine all the html codes.
-        legend.innerHTML = labels.join('') + source;
+        
 
 
         
